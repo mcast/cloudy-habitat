bdd8e15f5946b7ef66cc33745997cae438c77998
Matthew Astley <mca@sanger.ac.uk>
2019-11-04 16:34:05 +0000

    in Openstack Horizon cloud-init, replace the default user

--- WTSI-openstack/cloud-init.sh
+++ WTSI-openstack/cloud-init.sh
@@ -5,11 +5,21 @@ if [ $( ls -1 /home | wc -l ) != 1 ]; then
     exit 1
 fi
 
-luser=$( ls -1 /home | head -n1 )
-echo Seeing one less-privileged user named $luser
+fetch_proj() {
+    luser=$( ls -1 /home | head -n1 )
+    echo Seeing one less-privileged user named $luser
 
-su -c '
+    su -c '
  echo here in $PWD at $( date ) as $( whoami )
  cd
  git clone https://github.com/mcast/cloudy-habitat
 ' $luser
+}
+
+fetch_proj
+
+# replace the default user
+/home/$luser/cloudy-habitat/part/user-security.sh
+
+# fetch again, we destroyed the previous copy
+fetch_proj
