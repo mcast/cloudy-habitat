96689d7b925db2bd5e17c3b0e2ab726e6d460f4b
Matthew Astley <mca@sanger.ac.uk>
2019-11-05 10:51:31 +0000

    +pkg
    and tidy up after installs

--- start-with-sudo.sh
+++ start-with-sudo.sh
@@ -49,6 +49,7 @@ _deb_style() {
     sudo apt install -y aptitude
     # below: may require consideration of unmet dependency solutions and other prompts
     sudo aptitude install $( grep -hvE '^#' "$@" )
+    sudo aptitude clean
     set +x
 }
 
